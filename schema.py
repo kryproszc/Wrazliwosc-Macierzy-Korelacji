0739b7ca078b490126ebd4db3a15dc09888b868a
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-09-20 16:31:32 +0200

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,20 +1,7 @@
-Ustaw cache na istniejący katalog, np. na C:
-New-Item -ItemType Directory -Force -Path C:\npm-cache-temp
-npm config set cache C:\npm-cache-temp --global
-Wyczyść stan instalacji:
-Remove-Item node_modules -Recurse -Force -ErrorAction SilentlyContinue
+ove-Item node_modules -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item package-lock.json -Force -ErrorAction SilentlyContinue
-npm cache clean --force
-Zainstaluj ponownie:
-npm install --cache C:\npm-cache-temp --no-audit --no-fund
-Potem uruchom:
+Instalacja z obejściem peer-resolvera
+npm install --legacy-peer-deps --no-audit --no-fund
+Potem
 npm run build
 npm run start
-Jeśli dalej padnie:
-
-Sprawdź połączenie z rejestrem:
-npm ping
-Usuń ewentualne stare proxy:
-npm config delete proxy
-npm config delete https-proxy
-Powtórz npm install z cache na C:.
